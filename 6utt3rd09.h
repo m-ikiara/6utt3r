07caaeb4a96a848069a0fb9e1e0eea2d1ef5625d
brian-ikiara <brnikiara@gmail.com>
2023-06-22 01:14:40 +0300

    Add image_present.cpp that renders graphics; Update *.h

--- 6utt3rd09.h
+++ 6utt3rd09.h
@@ -4,12 +4,21 @@
 
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <unistd.h>
 #include <stddef.h>
 #include <stdlib.h>
 
+/* Global variables */
+const int WIDTH = 800;
+const int HEIGHT = 600;
+SDL_Window *gWindow = NULL;
+SDL_Surface *gScreenSurface = NULL;
+SDL_Surface *gHelloWorld = NULL;
+
 /* Main Prototypes */
-void game_loop();
-void render_graphics();
+bool init();
+bool loadMedia();
+void close();
 
 #endif /* !_6UTT3RD09_H_ */
