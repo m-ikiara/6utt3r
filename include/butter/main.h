3ca9b4721b5054cd7bdb0e7eda80f56db87299f5
m-ikiara <brnikiara@gmail.com>
2024-02-09 16:25:48 +0300

    :x: To hell with OpenGL!
    I tried to use `glad` but it never worked and now I'm pissed! I want to
    now stick to pure SDL2 till other stuff happens. Wish me luck!
    
    TODO: Implement Surface Rendering for 6utt3r!

---
+++ include/butter/main.h
@@ -0,0 +1,30 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <SDL2/SDL.h>
+
+#define r (float) 1.0 
+#define b (float) 0.0 
+#define g (float) 0.0 
+#define a (float) 1.0 
+
+#define x 20
+#define y 20
+#define w 800
+#define h 600
+
+/**
+ * @brief: To be set if using OpenGL
+ *
+ * #define gl_x (GLint) 20
+ * #define gl_y (GLint) 20
+ * #define gl_w (GLsizei) 800
+ * #define gl_h (GLsizei) 600
+ */
+
+#endif // !_MAIN_H_
--- src/main.c
+++ src/main.c
@@ -1,8 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <SDL2/SDL.h>
-#include <glad/glad.h>
+#include <butter/main.h>
 
 int
 main(int argc, char *argv[]) {
@@ -12,82 +8,53 @@ main(int argc, char *argv[]) {
   SDL_Window *window = NULL;
 
   if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    printf("SDL failed to initialize: %s", SDL_GetError());
+    fprintf(stderr, "SDL failed to initialize: %s", SDL_GetError());
   else
-    printf("[SUCCESS] Hello, World!");
-
-  // Setting up OpenGL version
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-  SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+    fprintf(stderr, "[SUCCESS] Hello, World!");
 
   window = SDL_CreateWindow("6utt3r: The Ray-casting Engine",
-                            (int) 800 / 2,
-                            (int) 400 / 2,
-                            800,
-                            600,
+                            x, y, w, h,
                             SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
   SDL_Delay(3000);
 
-  !window ? printf("[ERROR] Failed to Initialize Window!: %s", SDL_GetError())
-          : printf("[SUCCESS] Window created!: %p", (void *) &window);
+  !window ? fprintf(stderr,
+                    "[ERROR] Failed to Initialize Window!: %s",SDL_GetError())
+          : fprintf(stderr, "[SUCCESS] Window created!");
 
-  // Pre-load the Graphics:
-  // Context
-  SDL_GLContext context;
-  context = SDL_GL_CreateContext(window);
-
-  // Function pointers
-  gladLoadGLLoader(SDL_GL_GetProcAddress);
-  
   bool isRunning = true;
   while (isRunning) {
-    // Refreshing the Viewport
-    glViewport((GLint) 800 / 2,
-               (GLint) 400 / 2,
-               800, 600);
-
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
       event.type == SDL_QUIT ? isRunning = false : isRunning;
       if (event.type == SDL_MOUSEMOTION)
-        printf("[INFO] Mouse's moved!");
+        fprintf(stderr, "[INFO] Mouse's moved!");
       if (event.type == SDL_KEYDOWN) {
         if (event.key.keysym.sym == SDLK_0)
-          printf("[INFO] You pressed 0!");
+          fprintf(stderr, "[INFO] You pressed 0!");
         else
-          printf("[INFO] You didn't press 0!");
+          fprintf(stderr, "[INFO] You didn't press 0!");
       }
       if (event.type == SDL_KEYUP) {
         if (event.key.keysym.sym == SDLK_0)
-          printf("[INFO] You stopped pressing 0!");
+          fprintf(stderr, "[INFO] You stopped pressing 0!");
       }
       
       const Uint8 *state = SDL_GetKeyboardState(NULL);
       if (state[SDL_SCANCODE_RIGHT])
-        printf("[INFO] Turning right...\n");
+        fprintf(stderr, "[INFO] Turning right...\n");
       if (state[SDL_SCANCODE_LEFT])
-        printf("[INFO] Turning left...\n");
+        fprintf(stderr, "[INFO] Turning left...\n");
     }
-
-    // Rendering color to the Viewport
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-    glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-
-    // Updating changes to Viewport
-    SDL_GL_SwapWindow(window);
   }
 
   SDL_DestroyWindow(window);
 
   for (int i = 5; i > 0; i--) {
-    printf("[INFO] Closing in: %d", i);
+    fprintf(stderr, "[INFO] Closing in: %d", i);
     SDL_Delay(3000);
   }
 
-  printf("Bye Bye! =-D");
+  fprintf(stderr, "Bye Bye! =-D");
   SDL_Quit();
 
   return EXIT_SUCCESS;
