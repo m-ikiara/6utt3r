24f58563b0dd87ad718313c4583e6c620b24df29
m-ikiara <brnikiara@gmail.com>
2024-02-10 16:22:59 +0300

    :sparkles: Implemented Surface modifier!
    This implementation features a means of drawing pixels onto the screen
    as well as a few additions to the header file. When pressing LMB and
    RMB, a red and blue pixel is activated, respectively. This took some
    time to implement, but I'm pretty stoked we had something working.
    Cheers.
    
    TODO: Implement a Render and Draw function.

--- include/butter/main.h
+++ include/butter/main.h
@@ -10,15 +10,55 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-#define r (float) 1.0 
-#define b (float) 0.0 
-#define g (float) 0.0 
-#define a (float) 1.0 
+/**
+ * @brief The Vectors we'll be using in the Development of Butter
+ */
+typedef struct Vector1D {
+  int *x0;
+  int *y0;
+} vec1;
 
-#define w 800
-#define h 600
-#define x (int) (w / 2)
-#define y (int) ((h * 1) / 3)
+typedef struct Vector2D {
+  int *x0;
+  int *y0;
+  int *x1;
+  int *y1;
+} vec2;
+
+typedef struct Vector3D {
+  int *x0;
+  int *y0;
+  int *z0;
+  int *x1;
+  int *y1;
+  int *z1;
+} vec3;
+
+/**
+ * @brief Color Settings
+ */
+typedef struct {
+  uint8_t *pr;
+  uint8_t *pg;
+  uint8_t *pb;
+} pColor;
+
+/**
+ * @brief Window Settings
+ */
+typedef struct {
+  int *x;
+  int *y;
+  int *w;
+  int *h;
+} setWindow;
+
+/**
+ * @brief Custom functions to interface with SDL2
+ */
+void SetPixel(SDL_Surface *,              // Surface or Screen
+              int, int,                   // Pixel position (x, y)
+              uint8_t, uint8_t, uint8_t); // Pixel color (r, g, b)
 
 /**
  * @brief: To be set if using OpenGL
@@ -27,6 +67,11 @@
  * #define gl_y (GLint) 20
  * #define gl_w (GLsizei) 800
  * #define gl_h (GLsizei) 600
+ *
+ * #define r (float) 1.0 
+ * #define b (float) 0.0 
+ * #define g (float) 0.0 
+ * #define a (float) 1.0 
  */
 
 #endif // !_MAIN_H_
--- src/main.c
+++ src/main.c
@@ -1,71 +1,143 @@
+/*******************************6uttt3r**************************************
+ *
+ * TODO: Read through each of these and work accordingly:
+ *   1. Use the Vectors and Color structs that we had already defined
+ *   2. Actually draw an object to the Screen
+ *   3. Refactor the code and make it more pleasant
+ *
+ ***************************************************************************/
 #include <butter/main.h>
 
+/**
+ * @brief Draws a pixel to the Screen
+ *
+ * @param (SDL_Surface *) surface: Pointer to Screen
+ * @param (int) x
+ * @param (int) y
+ * @param (uint8_t) r
+ * @param (uint8_t) g
+ * @param (uint8_t) b
+ *
+ * @description: This function will set the value of a given pixel in order to
+ * display it on our window. The pixelArray depends on a given processor's
+ * architecture among other nitty-gritties. Feel free to modify the value
+ * of the offsets to your liking.
+ * @returns: Nothing
+ */
+void
+SetPixel(SDL_Surface *surface, int x, int y, uint8_t r, uint8_t g, uint8_t b)
+{
+  SDL_LockSurface(surface);
+  uint8_t *pixelArray = (uint8_t *)surface->pixels;
+
+  pixelArray[y*surface->pitch + x*surface->format->BytesPerPixel + 0] = b;
+  pixelArray[y*surface->pitch + x*surface->format->BytesPerPixel + 1] = g;
+  pixelArray[y*surface->pitch + x*surface->format->BytesPerPixel + 2] = r;
+
+  SDL_UnlockSurface(surface);
+}
+
 int
-main(int argc, char *argv[]) {
+main(int argc, char *argv[])
+{
   if (argc == 0 && !argv)
     return EXIT_FAILURE;
 
+  // WindowSettings *w1 = NULL;
   SDL_Window *window = NULL;
   SDL_Surface *screen = NULL; // "Grab" the Screen
 
   SDL_Init(SDL_INIT_VIDEO) < 0 ? fprintf(stderr, "[ERROR] %s", SDL_GetError())
-                               : fprintf(stdout, "[INFO] Hi, I'm Butter! \\o/");
+                               : fprintf(stderr, "[INFO] Hi, I'm Butter! \\o/");
 
+  // *w1->x = (int) 800 / 2, *w1->y = (int) 400 / 2;
+  // *w1->w = 800, *w1->h = 600;
   window = SDL_CreateWindow("6utt3r: The Ray-casting Engine",
-                            x, y, w, h,
+                            (int) 800 / 2, (int) 400 / 2,
+                            800, 600,
                             SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
   SDL_Delay(3000);
 
   !window ? fprintf(stderr, "[ERROR] %s", SDL_GetError())
-          : fprintf(stdout, "[INFO] Window Created! =-D");
+          : fprintf(stderr, "[INFO] Window Created! =-D");
 
-  // Initialize the Surface buffer
+  /**
+   * // Initialize the Surface buffer
+   *
+   * // Load the Image
+   * SDL_Surface *image;
+   * image = IMG_Load("./assets/images/favicon.png");
+   * SDL_BlitSurface(image, NULL, screen, NULL);
+   * SDL_FreeSurface(image);
+   * // Update to Screen
+   */
   screen = SDL_GetWindowSurface(window);
 
-  // Load the Image
-  SDL_Surface *image;
-  image = IMG_Load("./assets/images/favicon.png");
-  SDL_BlitSurface(image, NULL, screen, NULL);
-  SDL_FreeSurface(image);
-
-  // Update to Screen
-  SDL_UpdateWindowSurface(window);
 
   bool isRunning = true;
   while (isRunning) {
     SDL_Event event;
+
+    /**
+     * // Capture Mouse state and Position
+     * pColor *pc1 = NULL, *pc2 = NULL;
+     * Vector1 *v1 = NULL;
+     * *pc1->pr = 0,   *pc1->pg = 0, *pc1->pb = 255;
+     * *pc2->pr = 255, *pc2->pg = 0, *pc2->pb = 0;
+     * *v1->x0 = 0, *v1->y0 = 0;
+     */
+    int x, y;
+
+    // SDL_GetMouseState((*v1).x0, (*v1).y0);
+    SDL_GetMouseState(&x, &y);
+
     while (SDL_PollEvent(&event)) {
       event.type == SDL_QUIT ? isRunning = false : isRunning;
-      if (event.type == SDL_MOUSEMOTION)
-        fprintf(stdout, "[INFO] Mouse's moved!");
-      if (event.type == SDL_KEYDOWN) {
-        if (event.key.keysym.sym == SDLK_0)
-          fprintf(stdout, "[INFO] You pressed 0!");
-        else
-          fprintf(stdout, "[INFO] You didn't press 0!");
-      }
-      if (event.type == SDL_KEYUP) {
-        if (event.key.keysym.sym == SDLK_0)
-          fprintf(stdout, "[INFO] You stopped pressing 0!");
-      }
       
-      const Uint8 *state = SDL_GetKeyboardState(NULL);
-      if (state[SDL_SCANCODE_RIGHT])
-        fprintf(stdout, "[INFO] Turning right...\n");
-      if (state[SDL_SCANCODE_LEFT])
-        fprintf(stdout, "[INFO] Turning left...\n");
+      // Query Mouse Position per Mouse Button click
+      if (event.button.button == SDL_BUTTON_LEFT) {
+        /**
+        fprintf(stderr,
+                "[MOUSE] Right MSB clicked at x: %d y: %d", *v1->x0, *v1->y0);
+        SetPixel(screen, *v1->x0, *v1->y0, *pc1->pr, *pc1->pg, *pc1->pb);
+        */
+        fprintf(stderr, "MOUSE] Left MSB clicked at x: %d y: %d\n", x, y);
+        SetPixel(screen, x, y, 255, 0, 0);
+      }
+
+      if (event.button.button == SDL_BUTTON_RIGHT) {
+        /**
+        fprintf(stderr,
+                "[MOUSE] Right MSB clicked at x: %d y: %d", *v1->x0, *v1->y0);
+        SetPixel(screen, *v1->x0, *v1->y0, *pc2->pr, *pc2->pg, *pc2->pb);
+         */
+        fprintf(stderr, "MOUSE] Right MSB clicked at x: %d y: %d\n", x, y);
+        SetPixel(screen, x, y, 0, 0, 255);
+      }
     }
+
+    // Refresh the Screen per Update
+    SDL_UpdateWindowSurface(window);
   }
 
   SDL_DestroyWindow(window);
 
   for (int i = 3; i > 0; i--) {
-    fprintf(stdout, "[INFO] Closing in %d", i);
+    fprintf(stderr, "[INFO] Closing in %d\n", i);
     SDL_Delay(3000);
   }
+  fprintf(stderr, "\n");
 
-  fprintf(stdout, "[INFO] Bye Bye! =-D");
+  fprintf(stderr, "[INFO] Bye Bye! =-D\n");
   SDL_Quit();
 
   return EXIT_SUCCESS;
 }
+
+/*******************************6utt3r***************************************
+ *
+ * @name 6utt3r
+ * @version 0.3.0
+ * @author Brian M'Ikiara
+ *
+ ***************************************************************************/
