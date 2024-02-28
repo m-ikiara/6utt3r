92896712ebbb77ecbc2e05cf8ecbf27953576caf
m-ikiara <brnikiara@gmail.com>
2024-02-28 11:18:03 +0300

    feat: (refactor #52) Opted for simplicity and no emojis
    Review #1:
    So it turns out that emojis are pretty unprofessional and made my
    Commit messages weird and misunderstood. I need to ensure that my
    Commits follow SemVer and Conventional Commits style for the sake of the
    people who'll be reviewing my work. So this is goodbye to emojis and the
    Ikiara Commit Style Guide!
    
    Review #52:
    The SIGSEGV and Linking Issues were caused by the "Complex" & "Uninformed"
    Abstraction that was originally implemented. I hope that this commit
    will be a reflection of this simplified C code that should work without
    error. Cheers!
    
    TODO: - Render a Textured Rectangle onto the Window.
          - Reorganize the code and make it SOLID & DRY.

--- src/main.c
+++ src/main.c
@@ -5,7 +5,200 @@
  *
  ***************************************************************************/
 
-#include "../include/btr/btr_main.h"
+#include <btr/btr_main.h>
+
+/**
+ * @brief Initializes the Renderer
+ *
+ * @param (void)
+ */
+SDL_Renderer *
+init_render(SDL_Window *window, int i)
+{
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, i,
+                                              SDL_RENDERER_ACCELERATED);
+  return renderer;
+}
+
+/**
+ * @brief Draws a pixel to the Screen
+ *
+ * @param (SDL_Surface *) surface
+ * @param (int) x
+ * @param (int) y
+ * @param (uint8_t) r
+ * @param (uint8_t) g
+ * @param (uint8_t) b
+ *
+ * @description This function will set the value of a given pixel in order to
+ * display it on our window. The pixel array depends on a given processor's
+ * architecture among other nitty-gritties. Feel free to modify the value
+ * of the offsets to your liking.
+ * @returns Nothing
+ */
+void
+set_pixel(SDL_Surface *surface, int x, int y, uint8_t r, uint8_t g, uint8_t b)
+{
+  SDL_LockSurface(surface);
+  uint8_t *pixel_array = (uint8_t *)surface->pixels;
+
+  pixel_array[y*surface->pitch + x*surface->format->BytesPerPixel + 0] = b;
+  pixel_array[y*surface->pitch + x*surface->format->BytesPerPixel + 1] = g;
+  pixel_array[y*surface->pitch + x*surface->format->BytesPerPixel + 2] = r;
+
+  SDL_UnlockSurface(surface);
+}
+
+/**
+ * @brief Draw a Rectangle
+ *
+ * @param (SDL_Renderer *) renderer
+ * @param (Uint8) r
+ * @param (Uint8) g
+ * @param (Uint8) b
+ * @param (Uint8) a
+ *
+ * @returns Nothing
+ */
+void
+draw_rect(SDL_Renderer *renderer)
+{
+  const SDL_Rect rect = { rect_x0, rect_y0, rect_w0, rect_h0 };
+
+  SDL_SetRenderDrawColor(renderer, rect_r, rect_g, rect_b, rect_a);
+  SDL_RenderDrawRect(renderer, &rect);
+  SDL_SetRenderDrawColor(renderer, rect_r, rect_g, rect_b, rect_a);
+  SDL_RenderFillRect(renderer, &rect);
+
+  SDL_RenderPresent(renderer);
+}
+
+/**
+ * @brief Initializes SDL and 6utt3r as a whole
+ *
+ * @param (char *) title
+ * @param (int) x
+ * @param (int) y
+ * @param (int) w
+ * @param (int) h
+ *
+ * @returns A pointer to the window or NULL
+ */
+SDL_Window *
+init_butter(const char title[], int x, int y, int w, int h)
+{
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("[ERROR] %s", SDL_GetError());
+    fflush(stdout);
+  }
+
+  SDL_Window *window = SDL_CreateWindow(
+      title, x, y, w, h, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+  SDL_Delay(3000);
+
+  if (!window) {
+    printf("[ERROR] %s", SDL_GetError());
+    fflush(stdout);
+    return NULL;
+  }
+  printf("[INFO] Window Created! =-D");
+  fflush(stdout);
+
+  return window;
+}
+
+/**
+ * @brief Handles all of 6utt3r's events
+ *
+ * @param (SDL_Event) event
+ * @param (SDL_Surface *) surface
+ * @param (SDL_Renderer *) renderer
+ * @param (bool) status
+ *
+ * @returns Nothing
+ */
+bool *
+handle_butter(SDL_Event event,
+              SDL_Surface *surface, SDL_Renderer *renderer,
+              bool *status)
+{
+  switch (event.type) {
+    case SDL_QUIT:
+      printf("[QUIT] Exiting...");
+      fflush(stdout);
+      *status = false;
+      break;
+
+    case SDL_MOUSEBUTTONDOWN:
+      SDL_GetMouseState((int *) mouse_x, (int *) mouse_y);
+
+      if (event.button.button == SDL_BUTTON_LEFT) {
+        printf("[INFO] Left Button pressed! o-0");
+        fflush(stdout);
+        set_pixel(surface, mouse_x, mouse_y, 255, 0, 0);
+        *status = true;
+      }
+      if (event.button.button == SDL_BUTTON_RIGHT) {
+        printf("[INFO] Right Button pressed! 0-o");
+        fflush(stdout);
+        set_pixel(surface, mouse_x, mouse_y, 0, 0, 255);
+        *status = true;
+      }
+      break;
+
+    case SDL_KEYDOWN:
+      if (event.key.keysym.sym == SDLK_r) draw_rect(renderer);
+      if (event.key.keysym.sym == SDLK_x) {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+        SDL_RenderClear(renderer);
+      }
+      *status = true;
+      break;
+
+    default:
+      printf("[ERROR] %s", SDL_GetError());
+      fflush(stdout);
+      *status = true;
+      break;
+  }
+
+  return status;
+}
+
+/**
+ * @brief Update 6utt3r's state
+ *
+ * @param (SDL_Window *) window
+ *
+ * @returns Nothing
+ */
+void
+update_butter(SDL_Window *window, bool status)
+{
+  if (status == true) { SDL_UpdateWindowSurface(window); }
+}
+
+/**
+ * @brief Quits 6utt3r and closes SDL2's subsystems
+ *
+ * @param (SDL_Window *) window
+ *
+ * @returns Nothing
+ */
+void
+quit_butter(SDL_Window *window)
+{
+  SDL_DestroyWindow(window);
+
+  for (int i = 3; i > 0; i--) {
+    printf("[QUIT] Closing in %d", i);
+    fflush(stdout);
+  }
+
+  printf("[QUIT] Bye Bye! ^-^");
+  fflush(stdout);
+  SDL_Quit();
+}
 
 int
 main(int argc, char *argv[])
@@ -26,7 +219,6 @@ main(int argc, char *argv[])
     }
     update_butter(window, is_running);
   }
-
   quit_butter(window);
 
   return EXIT_SUCCESS;
